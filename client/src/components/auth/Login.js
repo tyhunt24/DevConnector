762f5364246fc6238025547e7f65091007327da7
Ty Hunt <jtmhunt22@gmail.com>
2020-09-21 11:44:03 -0600

    Styles on Login and Register form complete

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react'
+import {Link} from "react-router-dom"
 
 const Login = () => {
+       //initial useState hook setup 
+       const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    //destructor the form data for use state 
+    const {email, password} = formData; 
+
+    //allows us to make changes
+    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
+
+    //submit the data the user enters
+    const onSubmit = e => {
+      e.preventDefault()
+      console.log(formData)
+    }
     return (
-        <div>
-            Login
-        </div>
+        <Fragment>
+            <h1 className="large text-primary">Login</h1>
+            <p className="lead"><i className="fas fa-user"></i> Sign in to your account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input type="email" placeholder="Email" name="email" value={email} onChange={e => onChange(e)} />
+                </div>
+                <div className="form-group">
+                    <input type="password" placeholder="Password" name="password" value={password}  onChange={e => onChange(e)} />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/register">Register</Link>
+            </p>
+        </Fragment>
     )
 }
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react'
+import {Link} from "react-router-dom"
 
 const Register = () => {
-    return (
-     <div>
-         Register
-     </div>
+    //initial useState hook setup 
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: ""
+    });
+
+    //destructor the form data for use state 
+    const {name, email, password, password2} = formData; 
+
+    //allows us to make changes
+    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value})
+
+    //submit the data the user enters
+    const onSubmit = e => {
+        e.preventDefault()
+        if(password !== password2) {
+            console.log("Passwords do not match")
+        } else {
+            console.log(formData)
+        }
+    }
+    
+    return ( 
+        <Fragment>
+            <h1 className="large text-primary">Sign Up</h1>
+            <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} />
+                </div>
+                <div className="form-group">
+                    <input type="email" placeholder="Email Address" name="email" value={email}  onChange={e => onChange(e)} />
+                    <small className="form-text">This site uses Gravatar so if you want a profile picture
+                    use a gravatar email</small>
+                </div>
+                <div className="form-group">
+                    <input type="password" placeholder="Password" name="password" minLength="6" value={password}  onChange={e => onChange(e)} />
+                </div>
+                <div className="form-group">
+                    <input type="password" placeholder="Confirm Password" name="password2" minLength="6" value={password2}  onChange={e => onChange(e)} />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Register" />
+            </form>
+            <p className="my-1">
+                Already have an account? <Link to="/login">Login</Link>
+            </p>
+        </Fragment>
+    
     )
 }
 
