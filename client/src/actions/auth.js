10096e6f3a88cac5403b5221bd7f5d23031fd2f7
Ty Hunt <jtmhunt22@gmail.com>
2020-09-23 15:31:56 -0600

    Able to register a user on the frontend

---
+++ client/src/actions/auth.js
@@ -0,0 +1,36 @@
+import axios from "axios"
+import {REGISTER_SUCCESS, REGISTER_FAIL} from "./types"
+import {setAlert} from "./alert"
+
+// register a user
+export const register = ({name, email, password}) => async dispatch => {
+    const config = {
+        headers: {
+            "Content-Type": 'application/json'
+        }
+    }
+    // ! turns it json
+    const body = JSON.stringify({name, email, password})
+    
+    try {
+        // send the registration to the database
+        const res = await axios.post("/api/users", body, config);
+         dispatch({
+            type: REGISTER_SUCCESS,
+            payload: res.data //returns the token
+        })
+    } catch (err) {
+        // gets the errors from the response
+        const errors = err.response.data.errors
+
+        //if there is an error
+        if(errors) {
+            //loop through the errors and each error have a message and the alert type of danger
+            errors.forEach(err => dispatch(setAlert(err.msg, 'danger')))
+        }
+
+        dispatch({
+            type: REGISTER_FAIL
+        })
+    }
+}
---
+++ client/src/reducers/auth.js
@@ -0,0 +1,34 @@
+import {REGISTER_SUCCESS, REGISTER_FAIL} from "../actions/types"
+
+const initialState = {
+    token: localStorage.getItem('token'), // we first get local storage
+    isAuthenticated: null, // No users yet so NOT authenticated
+     loading: true, // loading waiting for user to be authenticated
+    user: null // user has not yet been entered
+}
+
+export default function(state = initialState, action) {
+    const {type, payload} = action 
+
+    switch(type) {
+        case REGISTER_SUCCESS:
+            localStorage.setItem('token', payload.token);
+            return {
+                ...state, // we get whatever was in the state
+                ...payload, // we get everything from the payload
+                isAuthenticated: true, // the user is now authenticated
+                loading: false // we are not loading for the user anymore
+            }
+        case REGISTER_FAIL:
+            localStorage.removeItem("token")
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false
+            }
+
+        default: 
+          return state
+    }
+}
