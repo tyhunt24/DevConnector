e5c674e0e181df521408bcf1fedd6a17d6e8faf0
Ty Hunt <jtmhunt22@gmail.com>
2020-10-04 07:19:54 -0600

    made changes

---
+++ client/build/precache-manifest.ce11d9659d27c73d048e788f23b0043b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52a019e234342141dba7753d23060042",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19af20c076e8abfb8e20",
+    "url": "/static/css/main.407a0aa1.chunk.css"
+  },
+  {
+    "revision": "9b610eef18ac480e149a",
+    "url": "/static/js/2.f59c0415.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.f59c0415.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19af20c076e8abfb8e20",
+    "url": "/static/js/main.08587f67.chunk.js"
+  },
+  {
+    "revision": "38df0c5a41072eef99e33ec7955b32da",
+    "url": "/static/js/main.08587f67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "875a7bd5fef2153cc26adcf37ed177c8",
+    "url": "/static/media/showcase.875a7bd5.jpg"
+  }
+]);
